a8baa67d1e06472f1d2d4e6a56ca138494d29c59
havvtom <havvtom@gmail.com>
2019-11-21 14:26:20 +0200

    models

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/articles/{article}', 'ArticleController@show');
+
+Route::get('/videos/{video}', 'VideoController@show');
